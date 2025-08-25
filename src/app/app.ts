8f8b53960493d38e7c692a921a5c96acc1a8dc2c
Habeebah <ojuolapehabeebah@gmail.com>
2025-08-25 11:17:07 +0100

    Implement note management features with add, edit, and delete functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { noop } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Note-App');
+export class App implements OnInit {
+  
+  ngOnInit(){
+    const notes = localStorage.getItem('NoteBooks')
+    if(notes){
+      this.noteBooks = JSON.parse(notes)
+    }
+  }
+  noteTitle = ""
+  noteContent = ""
+  noteBooks: Array<{title: string, content:string}>= []
+  addNote(){
+    if(this.noteTitle == "" || this.noteContent ==""){
+      alert("Please Input All Field")
+    }else{
+      this.noteBooks.push({title: this.noteTitle, content: this.noteContent})
+      console.log(this.noteBooks);
+      localStorage.setItem('NoteBooks', JSON.stringify(this.noteBooks))
+      this.noteTitle = ""
+      this.noteContent = ""
+    }
+  }
+
+  delNote(index: number){
+    const removeItem = confirm("Are You Sure You Want To Delete This Note ")
+    if(removeItem == true){
+      this.noteBooks.splice(index, 1)
+      localStorage.setItem('NoteBooks', JSON.stringify(this.noteBooks))
+    }
+  }
+
+  editNote(){
+    alert('Working on me')
+  }
 }
