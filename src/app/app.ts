3d46173ad86cc8b7d681960968eec94ea2b532f9
Habeebah <ojuolapehabeebah@gmail.com>
2025-08-27 09:46:07 +0100

    Add color palette feature for note customization

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,37 +11,69 @@ import { noop } from 'rxjs';
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  
-  ngOnInit(){
+
+  ngOnInit() {
     const notes = localStorage.getItem('NoteBooks')
-    if(notes){
+    if (notes) {
       this.noteBooks = JSON.parse(notes)
     }
   }
+
+ canvas = false;
+ showCanvas() {
+   this.canvas = true;
+ }
+
+ noteColour = "#202124"
+ pickColour(item:any){
+  this.noteColour = item;
+ }
+
+ colors = ['#202124', '#77172E', '#692B17','#7C4A03','#264D3B', '#0C625D','#256377','#284255','#472E5B', '#6C394F', '#4b443A', '#232427']
+
   noteTitle = ""
   noteContent = ""
-  noteBooks: Array<{title: string, content:string}>= []
-  addNote(){
-    if(this.noteTitle == "" || this.noteContent ==""){
-      alert("Please Input All Field")
-    }else{
-      this.noteBooks.push({title: this.noteTitle, content: this.noteContent})
-      console.log(this.noteBooks);
-      localStorage.setItem('NoteBooks', JSON.stringify(this.noteBooks))
-      this.noteTitle = ""
-      this.noteContent = ""
+  noteBooks: Array<{ title: string, content: string, colour:any }> = []
+
+  constructor() {
+    document.addEventListener('mousedown', this.handleClickOutside.bind(this))
+  }
+  handleClickOutside(event: any) {
+    const box = document.querySelector('#start-typing') as HTMLElement;
+    if (box && !box.contains(event.target)) {
+      if (this.noteTitle !== "" || this.noteContent !== "") {
+        this.addNote();
+        this.noteTitle = "";
+        this.noteContent = "";
+        console.log(this.noteBooks);
+        // console.log("Clicked");
+      }
     }
   }
 
-  delNote(index: number){
+  enterPressed(event: any) {
+    if (event.key === "Enter") {
+      document.getElementById('noteArea')?.focus();
+    }
+  }
+  addNote() {
+    this.noteBooks.push({ title: this.noteTitle, content: this.noteContent, colour: this.noteColour })
+    console.log(this.noteBooks);
+    localStorage.setItem('NoteBooks', JSON.stringify(this.noteBooks))
+    this.noteTitle = ""
+    this.noteContent = ""
+  }
+  
+
+  delNote(index: number) {
     const removeItem = confirm("Are You Sure You Want To Delete This Note ")
-    if(removeItem == true){
+    if (removeItem === true) {
       this.noteBooks.splice(index, 1)
       localStorage.setItem('NoteBooks', JSON.stringify(this.noteBooks))
     }
   }
 
-  editNote(){
+  editNote() {
     alert('Working on me')
   }
 }
