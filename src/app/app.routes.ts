5abef50a21e4e696f09b30994e699057de11f75a
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-01 15:28:03 +0100

    Implement edit note functionality and create home component with associated styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { EditNote } from './edit-note/edit-note';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {path: 'edit-note', component:EditNote, title: 'Edit Note'},
+    {path: '**', redirectTo: ''}
+];
