8d239a3f6a11f6dca35dd912f73502f415cb6608
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-17 10:12:22 +0100

    Refactor signup component to implement reactive form with validation and enhance user feedback

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +12,16 @@ export class Signup {
 
   public builder =  inject(FormBuilder);
 
-  
+  signupForm = this.builder.group({
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]]
+  });
 
+
+ register(){
+   alert('Registered Successfully');
+ }
 
 }
